2210fc604ab3b032467c5127e2e95c09dd629617
lineeam <72556262+lineeam@users.noreply.github.com>
2022-05-11 22:03:49 +0200

    add incident skeletonpage

---
+++ Stix/Stix/Pages/Incident.cshtml.cs
@@ -0,0 +1,25 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stix.Pages
+{
+    public class IncidentModel : PageModel
+    {
+        private readonly ILogger<IncidentModel> _logger;
+
+        public IncidentModel(ILogger<IncidentModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss", new CultureInfo("en-NL"));
+            ViewData["IncidentName"] = "Example Incident";
+            ViewData["IncidentDate"] = dateTime;
+            ViewData["IncidentAdded"] = dateTime;
+            ViewData["IncidentSource"] = "";
+        }
+    }
+}
