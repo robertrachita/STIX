8b6c62660af21a7c98f8f08d47e50d8c84fe6474
Stefan Untura <u_stefan@mail.com>
2022-05-11 23:08:11 +0200

    User register, login page and model
    - Add user register page
    - Add user login page
    - Add user model
    - Add user register and login page to layout

---
+++ Stix/Stix/Models/UserModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stix.Models
+{
+    public class UserModel
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
