a60ba25f07cafdd0065938f268313905c57596bd
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-05-17 16:59:41 +0200

    somewhat working
    >doestnt throw anymore errors but returns empty array
    >update and delete and get by id dont work

--- Stix/Stix_Mongo_API/Program.cs
+++ Stix/Stix_Mongo_API/Program.cs
@@ -1,4 +1,5 @@
 using Stix_Mongo_API.Models;
+using Stix_Mongo_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<StixDatabaseSettings>(
     builder.Configuration.GetSection("StixDatabase"));
 
-builder.Services.AddSingleton<StixDatabaseSettings>();
+builder.Services.AddSingleton<UserService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
