235abd936b96ecbcc8897a1180fc2fc81ad65bdf
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-05-17 15:49:57 +0200

    Revert "fixed page structures"
    This reverts commit 35ac922f6d675a2363748b86d92bf80af3a268e1.

--- Stix/Stix/Pages/LoginUser.cshtml.cs
+++ Stix/Stix/Pages/LoginUser.cshtml.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stix.Models;
 
 namespace Stix.Pages
 {
-    public class RegisterModel : PageModel
+    public class LoginUser : PageModel
     {
         public void OnGet()
+        {
+        }
+
+        public void OnPost()
         {
         }
     }
