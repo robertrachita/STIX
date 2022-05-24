095f67ef15f741c2e495ea9d37dae5e865ba7bfe
Stefan Untura <u_stefan@mail.com>
2022-05-24 09:53:40 +0200

    Import-excel-file-page: Working import excel file and conversion to json
    - Add nuget package Aspose.Cells
    - Add import of excel file
    - Add conversion of excel file and saving to json
    
    TO DO:
    - Switch import path
    - Make import based on file type and global name

--- Stix/Stix/Pages/Import.cshtml.cs
+++ Stix/Stix/Pages/Import.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Aspose.Cells;
 
 namespace Stix.Pages
 {
@@ -9,6 +10,20 @@ namespace Stix.Pages
         public IFormFile ExcelFile { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ExcelFile != null)
+            {
+                var fileName = Path.GetFileName(ExcelFile.FileName);
+                var workBook = new Workbook("input.xlsx");
+                workBook.Save("Output.json");
+                return RedirectToPage("Index");
+
+            }
+
+            return Page();
         }
     }
 }
