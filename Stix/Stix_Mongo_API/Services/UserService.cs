1ca307a04fb005039aa562ca3cc0f88bd15ecd8b
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-05-26 11:08:56 +0200

    API (#14)
    >working
    >only for users
    >need to change user model and fields

---
+++ Stix/Stix_Mongo_API/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Stix_Mongo_API.Models;
+
+namespace Stix_Mongo_API.Services
+{
+    public class UserService
+    {
+        public readonly IMongoCollection<User> _usersCollection;
+
+        public UserService(
+            IOptions<StixDatabaseSettings> stixDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(stixDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(stixDatabaseSettings.Value.DatabaseName);
+            
+            _usersCollection = mongoDatabase.GetCollection<User>(stixDatabaseSettings.Value.UsersCollectionName);
+        }
+
+        public async Task<List<User>> GetAllUsersAsync() =>
+            await _usersCollection.Find(new BsonDocument()).ToListAsync();
+
+        public async Task<User?> GetUserAsync(string id) =>
+            await _usersCollection.Find(user => user.Id == id).FirstOrDefaultAsync();
+
+        // public async Task<User?> GetUserAsync(string email, string password) => 
+        //await _usersCollection.Find(user => user.Email == email && user.Password == password).FirstOrDefaultAsync();
+
+        public async Task CreateUserAsync(User user) =>
+            await _usersCollection.InsertOneAsync(user);
+
+        public async Task UpdateUserAsync(string id, User userIn) =>
+            await _usersCollection.ReplaceOneAsync(user => user.Id == id, userIn);
+
+        public async Task DeleteUserAsync(string id) =>
+            await _usersCollection.DeleteOneAsync(user => user.Id == id);
+
+        public async Task<bool> UserExistsAsync(string id) =>
+            await _usersCollection.Find(user => user.Id == id).AnyAsync();
+
+       // public async Task<bool> UserExistsAsync(string email, string password) =>
+            //await _usersCollection.Find(user => user.Email == email && user.Password == password).AnyAsync();
+            
+        
+
+    }        
+}
