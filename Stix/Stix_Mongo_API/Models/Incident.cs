08c17bb6ad0e6246104576cc680d7191858ddf3a
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-06-01 18:18:03 +0200

    update incident bsonExtraElements

--- Stix/Stix_Mongo_API/Models/Incident.cs
+++ Stix/Stix_Mongo_API/Models/Incident.cs
@@ -3,11 +3,17 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Stix_Mongo_API.Models
 {
-    [BsonIgnoreExtraElements]
+    
     public class Incident
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
+
+        [BsonElement("Name")] 
+        public string? Name { get; set; }
+
+        [BsonExtraElements]
+        public BsonDocument? Additional { get; set; }
     }
 }
