c2ca6087b31b4d5336a40ef78a109d20c63b5432
christostevan <72568727+christostevan@users.noreply.github.com>
2022-06-07 08:52:38 +0100

    Moderator page (#18)
    * Navbar and background color
    
    They're still not finished
    
    * table responsive
    
    * Added sticky table headers, made page more responsive
    
    Need to adjust navbar to fit site colors still
    
    Co-Authored-By: christostevan <72568727+christostevan@users.noreply.github.com>
    
    * Moderator page (not done)
    
    * Search, Filter, & Sort Unfinished
    
    * update
    
    Co-authored-by: SuperChalupa97 <69051047+SuperChalupa97@users.noreply.github.com>
    Co-authored-by: Robert Rachita <72192997+robertrachita@users.noreply.github.com>

---
+++ Stix/Stix/Models/IncidentsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Stix.Models
+{
+    public class IncidentsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int Reference_Number { get; set; }
+    }
+}
