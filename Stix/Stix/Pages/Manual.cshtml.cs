b2d66974f885366c3620dd94c20361f5c8c6b343
naskodh09 <atanas910@gmail.com>
2022-06-07 11:32:04 +0200

    Form for manual incident upload

---
+++ Stix/Stix/Pages/Manual.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stix.Pages
+{
+    public class ManualModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
