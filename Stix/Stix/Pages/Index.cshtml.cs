094e8d7349962d50722988d11cdf0187fe67ad28
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-06-08 19:32:46 +0200

    somehwat

--- Stix/Stix/Pages/Index.cshtml.cs
+++ Stix/Stix/Pages/Index.cshtml.cs
@@ -16,7 +16,7 @@ namespace Stix.Pages
         {
             _logger = logger;
             incidentController = new IncidentController();
-            incidentList = IncidentController.Index();
+            GetIncidents();
         }
         
         [BindProperty]
@@ -36,6 +36,12 @@ namespace Stix.Pages
         public void OnPost() 
         { 
             
+        }
+        
+        public async void GetIncidents()
+        {
+            this.incidentList = await IncidentController.Index();
+            //return 
         }
 
     }
