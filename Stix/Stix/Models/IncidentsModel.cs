6daad270e5e0c0989e4d8e44667c1bbc7469bdaa
christostevan <72568727+christostevan@users.noreply.github.com>
2022-06-11 07:54:19 +0100

    Get all incidents from mongodb (locally)

--- Stix/Stix/Models/IncidentsModel.cs
+++ Stix/Stix/Models/IncidentsModel.cs
@@ -1,12 +1,22 @@
-﻿namespace Stix.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+namespace Stix.Models
 {
     public class IncidentsModel
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
         public string Name { get; set; }
+
+        [BsonElement("description")]
         public string Description { get; set; }
+
+        [BsonElement("month")]
         public int Month { get; set; }
+        
+        [BsonElement("year")]
         public int Year { get; set; }
-        public int Reference_Number { get; set; }
     }
 }
