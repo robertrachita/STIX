4568e3c320d458bd3641899c27bd459c21b8ee6c
christostevan <72568727+christostevan@users.noreply.github.com>
2022-06-13 11:47:06 +0100

    Filter (working)
    With local MongoDB database

--- Stix/Stix/Pages/Filter.cshtml.cs
+++ Stix/Stix/Pages/Filter.cshtml.cs
@@ -1,20 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Stix.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
 namespace Stix.Pages
 {
     public class FilterModel : PageModel
     {
-        [BindProperty]
-        public string referenceValue { get; set; }
-        public void OnGet()
-        {
+        public List<FiltersModel> _filters = new List<FiltersModel>();
 
+        [BindProperty (SupportsGet = true)]
+        public bool title { get; set; }
+
+        [BindProperty (SupportsGet = true)]
+        public bool description { get; set; }
+
+        [BindProperty (SupportsGet = true)]
+        public bool date { get; set; }
+        public async void OnGetAsync() 
+        {
+            try
+            {
+                MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+                IMongoDatabase database = dbClient.GetDatabase("Stix");
+                IMongoCollection<FiltersModel> collection = database.GetCollection<FiltersModel>("Filters");
+                var results = collection.Find(new BsonDocument());
+                this._filters = results.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
 
-        public async Task<IActionResult> OnPost(string referenceNumber)
+        [HttpPost]
+        public async Task<IActionResult> OnPost()
         {
-            return RedirectToPage("/Index", new {referenceNumber = referenceNumber});    
+            try
+            {
+                MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+                IMongoDatabase database = dbClient.GetDatabase("Stix");
+                IMongoCollection<FiltersModel> collection = database.GetCollection<FiltersModel>("Filters");
+                var filter = Builders<FiltersModel>.Filter.Eq("_id", "62a6e2451819b1a2a1f723a1");
+                var update = Builders<FiltersModel>.Update.Set("title", title).Set("description", description).Set("date", date);
+                collection.UpdateOne(filter, update);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+            return RedirectToPage("./Index");
         }
+
     }
 }
