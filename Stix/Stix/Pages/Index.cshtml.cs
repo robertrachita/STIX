d1f26afdd2a98a8e19fef91781761a460fc3a4fc
stefanuntura <56734208+stefanuntura@users.noreply.github.com>
2022-06-13 19:20:20 +0200

    Filter and search incidents functionality (#23)
    Filter and search feature

--- Stix/Stix/Pages/Index.cshtml.cs
+++ Stix/Stix/Pages/Index.cshtml.cs
@@ -1,36 +1,91 @@
-﻿using System.Globalization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stix.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Text.RegularExpressions;
 
 namespace Stix.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private MongoClient _client;
+
+        // Incidents fields
+        public List<IncidentsModel> infos = new List<IncidentsModel>();
+
+        // Filter function fields
+        public List<FiltersModel> _filters = new List<FiltersModel>();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _client = new MongoClient("mongodb://localhost:27017");
+            this.getFilter();
+            this.getIncidents();
+            /*            foreach(var item in infos)
+                        {
+                            Console.WriteLine($"{item.Name}: {item.Description}");
+                        }*/
         }
-        
-        [BindProperty]
-        public string incidentName { get; set; } = "Default name";
-        [BindProperty]
-        public string description { get; set; } = "This section describes the incident";
-        [BindProperty]
-        public string addedOn { get; set; } = "2022-02-22\n" +
-            "T69:42:00";
-        [BindProperty]
-        public string viewMore { get; set; } = "";
 
-        public void OnGet()
+        public async void getIncidents()
         {
-            
+            try
+            {
+                IMongoDatabase database = _client.GetDatabase("Stix");
+                IMongoCollection<IncidentsModel> collection = database.GetCollection<IncidentsModel>("Incidents");
+                //var results = await collection.FindAsync(_ => true); 
+                var results = collection.Find(new BsonDocument());
+                this.infos = results.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
 
-        public void OnPost() 
-        { 
-            
+        public async void getFilter()
+        {
+            try
+            {
+                IMongoDatabase database = _client.GetDatabase("Stix");
+                IMongoCollection<FiltersModel> collection = database.GetCollection<FiltersModel>("Filters");
+                var results = collection.Find(new BsonDocument());
+                this._filters = results.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
         }
+        /*
+         * Search function
+         * There are 4 ways to do this: MQL, BsonDocument, Builder, and LINQ & Mapping Classes
+         */
+        [HttpPost]
+        public async Task OnPostAsync()
+        {
+/*            if (string.IsNullOrEmpty(searchString))
+            {
+                this.getIncidents();
+                return;
+            }
+            IMongoDatabase database = _client.GetDatabase("Stix");
+            var collection = database.GetCollection<IncidentsModel>("Incidents");
+
+            //var query = new BsonRegularExpression(new Regex(searchString, RegexOptions.IgnoreCase));
+            //var builder = Builders<IncidentsModel>.Filter;
+            //var filter = builder.Regex("name", query);
+            var searchFilter = Builders<IncidentsModel>.Filter.Regex("name", new BsonRegularExpression(new Regex(searchString, RegexOptions.IgnoreCase)));
+            this.infos = await collection.Find(searchFilter).ToListAsync();
+
+            *//* var search = Builders<IncidentsModel>.Filter.Eq("title", "Germany");*//*
+
+            //var filter = collection.Where(x => x.Name.Equals("Hapag-Lloyd hit by spear-phishing attack in Hamburg, Germany"));
+            //this.infos = search.ToList();*/
+        }
+
     }
 }
