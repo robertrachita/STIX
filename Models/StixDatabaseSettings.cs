a89613c9c65d933dc2d51b07a1c3c24e1a25d8e5
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-06-18 00:09:01 +0200

    API (standalone)
    optimised for heroku docker deployment
    instructions in the dev manual

---
+++ Models/StixDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Stix_Mongo_API.Models
+{
+    public class StixDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string UsersCollectionName { get; set; } = null!;
+
+        public string IncidentsCollectionName { get; set; } = null!;
+    }
+}
