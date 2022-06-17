a36487cfdc8cc63fd4359d469a1b068eca40c020
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-06-17 22:59:52 +0200

    html changes

--- Stix/Stix/Pages/Import.cshtml.cs
+++ Stix/Stix/Pages/Import.cshtml.cs
@@ -29,72 +29,72 @@ namespace Stix.Pages
                     var workBook = new Workbook(fileNameWithoutExtension + ".xlsx");
                     Worksheet worksheet;
                     worksheet = workBook.Worksheets[0];
-                    string ss;
-                    string[] sss;
-                    int x;
-                    int counter = 2;
-                    for (int i = 2; i <= 152; i++)
-                    {
-                        string cell;
-                        cell = "U" + counter.ToString();
-                        ss = worksheet.Cells[cell].Value.ToString();
+                    //string ss;
+                    //string[] sss;
+                    //int x;
+                    //int counter = 2;
+                    //for (int i = 2; i <= 152; i++)
+                    //{
+                    //    string cell;
+                    //    cell = "U" + counter.ToString();
+                    //    ss = worksheet.Cells[cell].Value.ToString();
 
-                        ss = ss.Replace(Environment.NewLine, "|");
-                        ss = ss.Replace("\\", "");
-                        sss = ss.Split("|");
+                    //    ss = ss.Replace(Environment.NewLine, "|");
+                    //    ss = ss.Replace("\\", "");
+                    //    sss = ss.Split("|");
 
-                        x = sss.Length;
+                    //    x = sss.Length;
 
-                        for (int j = 0; j < x; j++)
-                        {
+                    //    for (int j = 0; j < x; j++)
+                    //    {
 
-                            sss[j] = "\"" + sss[j];
-                            sss[j] += "\"";
-                            if (j == 0)
-                            {
-                                sss[j] = "[" + sss[j];
-                            }
-                            if (j == (x - 1))
-                            {
-                                sss[j] += "]"; ;
-                            }
-                        }
+                    //        sss[j] = "\"" + sss[j];
+                    //        sss[j] += "\"";
+                    //        if (j == 0)
+                    //        {
+                    //            sss[j] = "[" + sss[j];
+                    //        }
+                    //        if (j == (x - 1))
+                    //        {
+                    //            sss[j] += "]"; ;
+                    //        }
+                    //    }
 
-                        ss = string.Join(" ", sss);
+                    //    ss = string.Join(" ", sss);
 
-                        worksheet.Cells[cell].PutValue(ss);
+                    //    worksheet.Cells[cell].PutValue(ss);
 
-                        //next cell
-                        cell = "V" + counter.ToString();
+                    //    //next cell
+                    //    cell = "V" + counter.ToString();
 
-                        ss = worksheet.Cells[cell].Value.ToString();
+                    //    ss = worksheet.Cells[cell].Value.ToString();
 
-                        ss = ss.Replace(Environment.NewLine, "|");
-                        ss = ss.Replace("\\", "");
-                        sss = ss.Split("|");
+                    //    ss = ss.Replace(Environment.NewLine, "|");
+                    //    ss = ss.Replace("\\", "");
+                    //    sss = ss.Split("|");
 
-                        x = sss.Length;
+                    //    x = sss.Length;
 
-                        for (int j = 0; j < x; j++)
-                        {
+                    //    for (int j = 0; j < x; j++)
+                    //    {
 
-                            sss[j] = "\"" + sss[j];
-                            sss[j] += "\"";
-                            if (j == 0)
-                            {
-                                sss[j] = "[" + sss[j];
-                            }
-                            if (j == (x - 1))
-                            {
-                                sss[j] += "]"; ;
-                            }
-                        }
+                    //        sss[j] = "\"" + sss[j];
+                    //        sss[j] += "\"";
+                    //        if (j == 0)
+                    //        {
+                    //            sss[j] = "[" + sss[j];
+                    //        }
+                    //        if (j == (x - 1))
+                    //        {
+                    //            sss[j] += "]"; ;
+                    //        }
+                    //    }
 
-                        ss = string.Join(" ", sss);
+                    //    ss = string.Join(" ", sss);
 
-                        worksheet.Cells[cell].PutValue(ss);
-                        counter++;
-                    }
+                    //    worksheet.Cells[cell].PutValue(ss);
+                    //    counter++;
+                    //}
                     workBook.Save("Output.json");
                     ViewData["ConvertedToJson"] = string.Format("Successfuly converted {0} and saved file to a JSON file", fileNameWithExtension);
 
