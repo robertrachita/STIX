8909c4ccb4612752a38910cfa6e246e337006e23
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-06-17 21:12:53 +0200

    import excel to json cell

--- Stix/Stix/Pages/Import.cshtml.cs
+++ Stix/Stix/Pages/Import.cshtml.cs
@@ -11,6 +11,13 @@ namespace Stix.Pages
         public void OnGet()
         {
         }
+        public static string GetCellValue(string fileName, string sheetName, string addressName)
+        {//Instantiating a Workbook object
+            Workbook workbook = new Workbook(fileName);//Obtaining the reference of the Active worksheet
+            Worksheet worksheet = workbook.Worksheets.GetSheetByCodeName(sheetName);//retrieve value from cell
+            string returnValue = worksheet.Cells[addressName].Value.ToString(); 
+            return returnValue;
+        }
 
         public IActionResult OnPost()
         {
@@ -27,6 +34,71 @@ namespace Stix.Pages
                     // Display successful conversion and save message with the file name in console
                     /* {TO DO}: MAKE IMPORT GLOBAL(NOT LOCAL DIRECTORY) */
                     var workBook = new Workbook(fileNameWithoutExtension + ".xlsx");
+                    Worksheet worksheet;
+                    worksheet = workBook.Worksheets[0];
+                    string cell, ss;
+                    string[] sss;
+                    int x;
+                    for (int i = 2; i <= 152; i++)
+                    {
+                        cell = "U" + i;
+                        ss = GetCellValue(fileNameWithoutExtension, "Gecorrigeerd", cell);
+
+                        ss = ss.Replace(Environment.NewLine, "|");
+                        ss = ss.Replace("\\", "");
+                        sss = ss.Split("|");
+
+                        x = sss.Length;
+
+                        for (int j = 0; j < x; j++)
+                        {
+
+                            sss[j] = "\"" + sss[j];
+                            sss[j] += "\"";
+                            if (j == 0)
+                            {
+                                sss[j] = "[" + sss[j];
+                            }
+                            if (j == (x - 1))
+                            {
+                                sss[j] += "]"; ;
+                            }
+                        }
+
+                        ss = string.Join(" ", sss);
+
+                        worksheet.Cells[cell].PutValue(ss);
+
+                        //next cell
+                        cell = "V" + i;
+
+                        ss = GetCellValue(fileNameWithoutExtension, "Gecorrigeerd", cell);
+
+                        ss = ss.Replace(Environment.NewLine, "|");
+                        ss = ss.Replace("\\", "");
+                        sss = ss.Split("|");
+
+                        x = sss.Length;
+
+                        for (int j = 0; j < x; j++)
+                        {
+
+                            sss[j] = "\"" + sss[j];
+                            sss[j] += "\"";
+                            if (j == 0)
+                            {
+                                sss[j] = "[" + sss[j];
+                            }
+                            if (j == (x - 1))
+                            {
+                                sss[j] += "]"; ;
+                            }
+                        }
+
+                        ss = string.Join(" ", sss);
+
+                        worksheet.Cells[cell].PutValue(ss);
+                    }
                     workBook.Save("Output.json");
                     ViewData["ConvertedToJson"] = string.Format("Successfuly converted {0} and saved file to a JSON file", fileNameWithExtension);
 
